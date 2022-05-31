73ee34df281fbe691bccd2c86529d3caaa390525
Hoda Mire <hodmire@WKMZT7208AD0.global.publicisgroupe.net>
2022-05-31 11:33:19 +0100

    update

---
+++ server/db/conn.js
@@ -0,0 +1,23 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://hfmire:Imd2HPuF1uCPZI8x@cluster0.guxr2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+var _db;
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("employees");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
